30f9673fd93b290cf50c9e7fec11593063de25ec
Fajar Sidik <fajarsidikz43@gmail.com>
2025-11-18 12:01:25 +0700

    update path

--- Services/NetworkPaths.cs
+++ Services/NetworkPaths.cs
@@ -13,9 +13,14 @@ namespace InjectServiceWorker.Services
         public const string invalidPath = @"D:\INJECT_TARIF\Invalid";
         public const string errorPath = @"D:\INJECT_TARIF\Error";
 
-        public const string sourcePath2 = @"D:\INJECT_TARIF_NEW";
-        public const string donePath2 = @"D:\INJECT_TARIF_NEW\Done";
-        public const string invalidPath2 = @"D:\INJECT_TARIF_NEW\Invalid";
-        public const string errorPath2 = @"D:\INJECT_TARIF_NEW\Error";
+        //public const string sourcePath2 = @"D:\INJECT_TARIF_NEW";
+        //public const string donePath2 = @"D:\INJECT_TARIF_NEW\Done";
+        //public const string invalidPath2 = @"D:\INJECT_TARIF_NEW\Invalid";
+        //public const string errorPath2 = @"D:\INJECT_TARIF_NEW\Error";
+
+        public const string sourcePath2 = @"\\cluster-nas\FTP\NON FTP\\UPLOAD_TARIF";
+        public const string donePath2 = @"\\cluster-nas\FTP\NON FTP\\UPLOAD_TARIF\Done";
+        public const string invalidPath2 = @"\\cluster-nas\FTP\NON FTP\\UPLOAD_TARIF\Invalid";
+        public const string errorPath2 = @"\\cluster-nas\FTP\NON FTP\\UPLOAD_TARIF\Error";
     }
 }
